28ccf6e4a37dd4b3eba4e1ece5a46fb2f98b74f7
Anand Narayan <i@codeanand.com>
2023-11-07 15:58:58 -0800

    stream image files via cat and then pipe it

--- index.ts
+++ index.ts
@@ -1,58 +1,90 @@
-import * as fs from 'fs';
-import * as base64 from 'base-64';
-import axios from 'axios';
+#!/usr/bin/env node
+
+import * as fs from "fs";
+import * as base64 from "base-64";
+import axios from "axios";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
 
 // OpenAI API Key
-const apiKey: string = process.env.OPENAI_API_KEY || '';
-
+const apiKey: string = process.env.OPENAI_API_KEY || "";
 
 // Function to encode the image
-function encodeImage(imagePath: string): string {
-    const imageBuffer = fs.readFileSync(imagePath);
-    return base64.encode(imageBuffer.toString('binary'));
+function encodeImage(imageBuffer: Buffer): string {
+  return base64.encode(imageBuffer.toString("binary"));
 }
 
-// Path to your image
-const imagePath: string = "./image.jpg";
+// Set up the CLI arguments
+const argv = yargs(hideBin(process.argv))
+  .option("file", {
+    alias: "f",
+    type: "string",
+    description: "Path to the image file",
+  })
+  .parseSync();
 
-// Getting the base64 string
-const base64Image: string = encodeImage(imagePath);
+// Read the image file from the path or from the input stream
+// Function to read image from file or stdin
+function getImageBuffer(): Promise<Buffer> {
+  return new Promise((resolve, reject) => {
+    if (argv.file) {
+      // Read image from file
+      fs.readFile(argv.file, (err, data) => {
+        if (err) reject(err);
+        else resolve(data);
+      });
+    } else {
+      // Read from stdin
+      const chunks: Buffer[] = [];
+      process.stdin.on("data", (chunk) => chunks.push(Buffer.from(chunk)));
+      process.stdin.on("end", () => resolve(Buffer.concat(chunks)));
+      process.stdin.on("error", (err) => reject(err));
+    }
+  });
+}
 
-const headers = {
+getImageBuffer().then((imageBuffer) => {
+  const base64Image: string = encodeImage(imageBuffer);
+
+  const headers = {
     "Content-Type": "application/json",
-    "Authorization": `Bearer ${apiKey}`
-};
+    Authorization: `Bearer ${apiKey}`,
+  };
 
-const payload = {
-    "model": "gpt-4-vision-preview",
-    "messages": [
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": "What’s in this image?"
-                },
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": `data:image/jpeg;base64,${base64Image}`
-                    }
-                }
-            ]
-        }
+  const payload = {
+    model: "gpt-4-vision-preview",
+    messages: [
+      {
+        role: "user",
+        content: [
+          {
+            type: "text",
+            text: "What’s in this image?",
+          },
+          {
+            type: "image_url",
+            image_url: {
+              url: `data:image/jpeg;base64,${base64Image}`,
+            },
+          },
+        ],
+      },
     ],
-    "max_tokens": 300
-};
+    max_tokens: 300,
+  };
 
-axios.post("https://api.openai.com/v1/chat/completions", payload, { headers: headers })
-    .then(response => {
-        if (response.data.choices) {
-            console.log(response.data.choices[0].message.content);
-        } else {
-            console.log('No choices found in the response.');
-        }
+  axios
+    .post("https://api.openai.com/v1/chat/completions", payload, {
+      headers: headers,
     })
-    .catch(error => {
-        console.error(error);
+    .then((response) => {
+      if (response.data.choices) {
+        console.log(response.data.choices[0].message.content);
+      } else {
+        console.log("No choices found in the response.");
+      }
+    })
+    .catch((error) => {
+      console.error(error);
     });
+});
